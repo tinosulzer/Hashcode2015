d85f2f3f8dd92f392adcdad10c8c81a633187392
Matt Ridley <matthew.ridley@physics.ox.ac.uk>
2021-01-23 18:09:22 +0000

    Added basic random solution

--- random_solution.py
+++ random_solution.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import List, Tuple, Dict
 import matplotlib.pyplot as plt
+import random
+random.seed(1993)
 
 @dataclass
 class Problem:
@@ -52,21 +54,74 @@ class Balloon(object):
         self.R = R
         self.C = C
         self.A = A
+        self.is_live = True
 
-    def move(self, instruction):
+    def move(self, instruction, update=True):
+        if not self.is_live:
+            if update:
+                self.instructions.append(instruction)
+            return self.position, self.altitude
+
         assert -1 <= instruction <= 1
         if self.altitude < 2:
             assert instruction > -1
         if self.altitude == self.A - 1:
             assert instruction < 1
-        self.altitude += instruction
-        self.instructions.append(instruction)
-        wind_vec = self.wind_vectors[(self.altitude, *self.position)]
-        self.position = (self.position[0] + wind_vec[0], (self.position[1] + wind_vec[1]))
+        new_altitude = self.altitude + instruction
+        wind_vec = self.wind_vectors[(*self.position, new_altitude)]
+        new_position = ((self.position[0] + wind_vec[0]) % self.C, (self.position[1] + wind_vec[1]))
+        if update:
+            self.position = new_position
+            self.altitude = new_altitude
+            self.instructions.append(instruction)
+
+            if self.is_live and not 0 <= self.position[1] < self.R:
+                self.is_live = False
+
+        return new_position, new_altitude
+
+def random_solution(problem):
+    balloons = [
+        Balloon(problem.starting_cell, problem.wind_vectors, problem.R, problem.C, problem.A)
+        for _ in range(problem.B)
+    ]
+    for turn in range(problem.T):
+        for balloon in balloons:
+            if balloon.altitude < 2:
+                possible = [0, 1]
+            elif balloon.altitude == problem.A-1:
+                possible = [-1, 0]
+            else:
+                possible = [-1, 0, 1]
+            instruction = random.choice(possible)
+            balloon.move(instruction)
+
+    instructions = []
+    for turn in range(problem.T):
+        instructions += [b.instructions[turn] for b in balloons]
+    return instructions
+
+
+def save_solution(instructions, filename):
+    with open(filename, "w") as f:
+        for instruction in instructions:
+            print(instruction, file=f)
+
+
+def test(problem):
+    balloon = Balloon(problem.starting_cell, problem.wind_vectors, problem.R, problem.C, problem.A)
+    balloon.move(1)
+    for i in range(problem.T-1):
+        print(balloon.position, balloon.altitude, balloon.is_live)
+        balloon.move(0)
 
 
 if __name__ == "__main__":
     problem = parse_input("hashcode_2015_final_round.in")
+    solution = random_solution(problem)
+    save_solution(solution, "random.txt")
+
+if False:
     plt.scatter([p[0] for p in problem.target_cells], [p[1] for p in problem.target_cells])
     altitude = 7
     x, y, u, v = [], [], [], []
@@ -78,4 +133,4 @@ if __name__ == "__main__":
             v.append(problem.wind_vectors[(xi, yi, altitude)][1])
     plt.quiver(x, y, u, v)
     plt.show()
-    print(problem)
+
